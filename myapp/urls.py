ee0c1d421823c52a3cd4b6d832a99f7803de481e
shivanianwekar97 <shivanianwekar97@gmail.com>
2020-07-24 18:39:41 +0530

    second commit

---
+++ myapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from myapp import views
+app_name="myapp"
+
+
+urlpatterns = [
+    
+    path('',views.index,name='index'),
+    path('home',views.home,name='home'),
+    path('fact/<n>',views.fact,name='fact'),
+    
+
+]
