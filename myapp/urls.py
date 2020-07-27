e8603142986b96ff30da3e79749909ba1e521e0a
shivanianwekar97 <shivanianwekar97@gmail.com>
2020-07-27 18:53:22 +0530

    third commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('home',views.home,name='home'),
     path('fact/<n>',views.fact,name='fact'),
-    
+    path('child/',views.child,name="child"),
 
 ]
